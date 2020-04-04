56fb848b4b275c4fd0b02f9705adcfecd71d24af
Rapid10 <datagram33@gmail.com>
2020-04-04 13:32:49 +0200

    removed code from Lab3

--- app/src/main/java/pl/rapid/MainActivity.java
+++ app/src/main/java/pl/rapid/MainActivity.java
@@ -1,35 +1,17 @@
 package pl.rapid;
 
-import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.res.ResourcesCompat;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Random;
-
-import pl.rapid.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,62 +28,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                Toast.makeText(getApplicationContext(), "FAB has been clicked!", Toast.LENGTH_LONG).show();
             }
         });
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        switch (item.getTitle().toString()) {
-            case "Settings":
-                Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_LONG).show();
-            case "About":
-                Toast.makeText(getApplicationContext(), "About", Toast.LENGTH_LONG).show();
-            case "Extras":
-                Toast.makeText(getApplicationContext(), "Extras", Toast.LENGTH_LONG).show();
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void callLoginIntention(View view) {
-        Intent loginIntent = new Intent(this, LoginActivity.class);
-        startActivity(loginIntent);
-    }
-
-    public void callImageCaptureIntention(View view) {
-        Intent imageCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(imageCaptureIntent, REQUEST_IMAGE_CAPTURE);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Bundle extras = data.getExtras();
-        Bitmap imageBitmap = (Bitmap) extras.get("data");
-        ConstraintLayout lay = (ConstraintLayout) findViewById(R.id.app_layout);
-        lay.setBackground(new BitmapDrawable(getResources(), imageBitmap));
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    public void randomizeIconForButton(View view) throws IllegalAccessException {
-        final Button randomizeButton = (Button) view;
-        Field[] drawables = R.drawable.class.getFields();
-        int random = new Random().nextInt(drawables.length);
-        Drawable drawable = getResources().getDrawable(drawables[random].getInt(null), null);
-        randomizeButton.setBackground(drawable);
     }
 }
 
