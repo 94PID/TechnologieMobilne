ab947d4c01419480cb401d5da2d2648374e7f2ab
Rapid10 <datagram33@gmail.com>
2020-04-04 13:53:27 +0200

    Added ListView controll on layout and ArrayAddapter in mainActivity

--- app/src/main/java/pl/rapid/MainActivity.java
+++ app/src/main/java/pl/rapid/MainActivity.java
@@ -2,6 +2,8 @@ package pl.rapid;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +12,14 @@ import androidx.appcompat.widget.Toolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        target = new ArrayList<>();
+        String[] values = new String[]{
+                "Pies", "Kot", "Koń", "Gołąb", "Kruk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz",
+                "Jeż", "Karaluch"
+        };
+
+        target.addAll(Arrays.asList(values));
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(this.adapter);
 
         Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
 
